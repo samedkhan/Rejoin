cbb80bf3a79ae753406ea755b83c5272714c3823
samedkhan <samadakh@code.edu.az>
2019-12-18 07:01:48 +0400

    init

---
+++ Rejoin/Data/RejoinDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Rejoin.Models;
+
+namespace Rejoin.Data
+{
+    public class RejoinDbContext:DbContext
+    {
+        public RejoinDbContext(DbContextOptions<RejoinDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> MyProperty { get; set; }
+    }
+}
